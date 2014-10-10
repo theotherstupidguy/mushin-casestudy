f349b041035bd740e74d6fb0974155d861dbac8a
theotherstupidguy <theotherstupidguy@gmail.com>
2014-10-10 14:16:03 +0200

    next

--- gameon/mygames.rb
+++ gameon/mygames.rb
@@ -7,13 +7,15 @@ module Hackspree
       statment [:user, :visits, :good_page_one] do 
 	#activation GameOn::Points, opts, GameOn::Points::Params[:add] 
 	activation GameOn::Points, {:inc_by => 1}, {:add => 1}  
+	activation GameOn::Points, {:inc_by => 1}, {:add => 1}  
+	activation GameOn::Points, {:inc_by => 1}, {:add => 1}  
       end
     end
 
     context :bad_mayor do 
       statment [:user, :visits, :bad_page] do 
 	activation GameOn::Points, opts, GameOn::Points::Params[:remove] 
-	#activation GameOn::Points, opts,{:remove => 1}  
+	activation GameOn::Points, opts,{:remove => 1}  
       end
     end
   end
